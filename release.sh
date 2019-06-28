1887e9411c7412bb0076dbf65bd1d852b5f23949
Rafal Piekarski <ravbaker@gmail.com>
2019-06-28 10:27:10 +0200

    final script for releases

--- release.sh
+++ release.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 
+echo "Building binaries..."
 ./build.sh toggl-rounder
 
 VERSION=`go run . -version`
+echo "Releasing $VERSION"
 
-printf  "v$VERSION\n" | cat - CHANGELOG.md  > RELEASE_NOTE.md
-hub release create --edit -F RELEASE_NOTE.md -a build/toggl-rounder-darwin-amd64 -a build/toggl-rounder-linux-amd64 -a build/toggl-rounder-windows-amd64.exe ${VERSION}
+printf  "v$VERSION\n\n" | cat - CHANGELOG.md  > RELEASE_NOTE.md
+hub release create -F RELEASE_NOTE.md -a build/toggl-rounder-darwin-amd64 -a build/toggl-rounder-linux-amd64 -a build/toggl-rounder-windows-amd64.exe ${VERSION}
 rm RELEASE_NOTE.md
